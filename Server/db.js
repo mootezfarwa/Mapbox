e3b6138b6365db43550c8c04a91ea94c89badda7
Mootaz FARWA <mootaz.farwa@SCEET.TN>
2024-05-03 14:21:39 +0100

    pins

--- Server/db.js
+++ Server/db.js
@@ -1,10 +1,10 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
+  user: 'taha',
+  host: '192.168.79.129',
   database: 'postgres',
-  password: 'postgres',
+  password: 'secret123',
   port: 5432, // Default PostgreSQL port
 });
 
